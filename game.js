a74198e18720ebeccb33c3941a3149bc9674448a
iamshaylajade <shaylajade21@yahoo.com>
2023-11-12 18:58:48 -0500

    updated code on various screens

--- game.js
+++ game.js
@@ -7,72 +7,80 @@ const progressBarFull = document.querySelector('#progressBarFull')
 let currentQuestion = {}
 let acceptingAnswers = true
 let score = 0
+let timeleft = 90
 let questionCounter = 0
 let availableQuestions = []
 
 let questions = [
     {
-        question: 'placeholder for a question'
-        choice1:
-        choice2:
-        choice3:
-        choice4:
-        Answer:
+        question: 'placeholder for a question',
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        answer: '2'
     },
     {
-        question: 'placeholder for a question'
-        choice1:
-        choice2:
-        choice3:
-        choice4:
-        Answer:
+        question: 'placeholder for a question',
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        answer: '2'
     },
     {
-        question: 'placeholder for a question'
-        choice1:
-        choice2:
-        choice3:
-        choice4:
-        Answer:
+        question: 'placeholder for a question',
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        answer: '2'
     },
     {
-        question: 'placeholder for a question'
-        choice1:
-        choice2:
-        choice3:
-        choice4:
-        Answer:
+        question: 'placeholder for a question',
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        answer: '2'
     },
     {
-        question: 'placeholder for a question'
-        choice1:
-        choice2:
-        choice3:
-        choice4:
-        Answer:
-    }
+        question: 'placeholder for a question',
+        choice1: 'A',
+        choice2: 'B',
+        choice3: 'C',
+        choice4: 'D',
+        answer: '2'
+    },
 ]
 
 const SCORE_POINTS = 100
-const MAX QUESTIONS = 5
+const MAX_QUESTIONS = 5
 
 startGame = () => {
     questionCounter = 0
     score = 0
     availableQuestions = [...questions]
+    startimer ()
     getNewQuestion()
+}
+startimer = () => {
+    var timer= setInterval(function(){
+        timeleft--
+        document.getElementById("timer").textContent = timeleft
+    },1000)
 }
 
 getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/end.html')
+        return window.location.assign('./end.html')
     }
 
     questionCounter++
-    progressText.innerText = 'Question ${questionCounter} of ${MAX_QUESTIONS}'
-    progressBarFull.style.width = '${(questionCounter/MAX_QUESTIONS) * 100}%'
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
 
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
     currentQuestion = availableQuestions[questionsIndex]
@@ -97,23 +105,24 @@ choices.forEach(choice => {
         const selectedAnswer = selectedChoice.dataset['number']
         let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :
         'incorrect'
-
+        console.log(classToApply)
         if(classToApply === 'correct') {
             incrementScore(SCORE_POINTS)
         }
 
-        selectedChoice.parentElement.classlist.add(classToApply)
+        selectedChoice.parentElement.classList.add(classToApply)
 
         setTimeout(() => {
-            selectedChoice.parentElement.classlist.remove(classToApply)
+            selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
         }, 1000)
     })
 })
 
 incrementScore = num => {
+    console.log ('hello')
     score +=num
-    scoreText.innerText = score
+    scoreText.textContent = score
 }
 
 startGame()
